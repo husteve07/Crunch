ccd8d14ec52630e3a1fa63f4074ed09d942e48b5
husteve07 <husteve7@gmail.com>
2025-06-18 12:55:18 -0400

    - Added Animations for basic movements

---
+++ Source/Crunch/Private/Animations/CAnimInstance.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CAnimInstance.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UCAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	OwnerCharacter = Cast<ACharacter>(TryGetPawnOwner());
+	if (OwnerCharacter)
+	{
+		OwnerMovementComponent = OwnerCharacter->GetCharacterMovement();
+	}
+}
+
+void UCAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	if (OwnerCharacter)
+	{
+		Speed = OwnerCharacter->GetVelocity().Length();
+		FRotator BodyRot = OwnerCharacter->GetActorRotation();
+		FRotator BodyRotDelta = UKismetMathLibrary::NormalizedDeltaRotator(BodyRot, BodyPrevRot);
+		BodyPrevRot = BodyRot;
+
+		YawSpeed = BodyRotDelta.Yaw/DeltaSeconds;
+		SmoothedYawSpeed = UKismetMathLibrary::FInterpTo(SmoothedYawSpeed, YawSpeed, DeltaSeconds, YawSpeedSmoothLerpSpeed);
+	}
+
+	if (OwnerMovementComponent)
+	{
+		isJumping = OwnerMovementComponent->IsFalling();
+	}
+}
+
+void UCAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+}
---
+++ Source/Crunch/Private/Animations/CAnimInstance.h
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "Math/UnitConversion.h"
+#include "CAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API UCAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE float GetSpeed() const { return Speed; }
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE bool IsMoving() const { return Speed != 0.0f; }
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE bool IsNotMoving() const { return Speed == 0.0f; }
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE float GetYawSpeed() const { return YawSpeed; }
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE float GetSmoothedYawSpeed() const { return SmoothedYawSpeed; }
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE bool GetIsJumping() const { return isJumping; }
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE bool GetIsOnGround() const { return !isJumping; }
+private:
+	UPROPERTY()
+	class ACharacter* OwnerCharacter;
+
+	UPROPERTY()
+	class UCharacterMovementComponent* OwnerMovementComponent;
+
+	float Speed;
+	float YawSpeed;
+	float SmoothedYawSpeed;
+	bool isJumping;
+	
+	UPROPERTY(EditAnywhere, Category="Animation")
+	float YawSpeedSmoothLerpSpeed = 1.f;
+	
+	FRotator BodyPrevRot;
+};
