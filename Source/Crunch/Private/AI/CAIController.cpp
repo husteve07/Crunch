d5b5c96bb40c23bba0819171eab884d86cdab4f8
husteve07 <husteve7@gmail.com>
2026-01-19 08:06:58 -0500

    - Added AI and configured the AI TeamID and Sight Perception

---
+++ Source/Crunch/Private/AI/CAIController.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include "CAIController.h"
+
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AIPerceptionTypes.h"
+#include "Character/CCharacter.h"
+#include "Perception/AISenseConfig_Sight.h"
+
+
+ACAIController::ACAIController()
+{
+	AIPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>("AI Perception");
+	SenseConfig = CreateDefaultSubobject<UAISenseConfig_Sight>("Sight config");
+
+	SenseConfig->DetectionByAffiliation.bDetectEnemies = true;
+	SenseConfig->DetectionByAffiliation.bDetectFriendlies = false;
+	SenseConfig->DetectionByAffiliation.bDetectNeutrals = false;
+
+	SenseConfig->SightRadius = 1000.f;
+	SenseConfig->LoseSightRadius = 1200.f;
+
+	SenseConfig->SetMaxAge(5.f);
+
+	SenseConfig->PeripheralVisionAngleDegrees = 100.f;
+
+	AIPerceptionComponent->ConfigureSense(*SenseConfig);
+	
+}
+
+void ACAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	SetGenericTeamId(FGenericTeamId(0));
+
+	IGenericTeamAgentInterface* PawnTeamInterface = Cast<IGenericTeamAgentInterface>(InPawn);
+	if (PawnTeamInterface)
+	{
+		PawnTeamInterface->SetGenericTeamId(GetGenericTeamId());
+	}
+}
+
---
+++ Source/Crunch/Private/AI/CAIController.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "CAIController.generated.h"
+
+UCLASS()
+class CRUNCH_API ACAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ACAIController();
+
+	virtual void OnPossess(APawn* InPawn);
+
+private:
+	UPROPERTY(VisibleDefaultsOnly, Category="Perception")
+	class UAIPerceptionComponent* AIPerceptionComponent;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category="Perception")
+	class UAISenseConfig_Sight* SenseConfig;
+};
